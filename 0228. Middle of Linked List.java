dc1f51a0e68fa95d5a242ba753f973c7d1e2fd42
Claire Yang <34817835+apluscs@users.noreply.github.com>
2018-12-23 17:16:34 -0500

    Create 0228. Middle of Linked List.java

---
+++ 0228. Middle of Linked List.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: the head of linked list.
+     * @return: a middle node of the linked list
+     */
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
