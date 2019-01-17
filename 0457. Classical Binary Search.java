28269f3762c6e50d2b6d5bc47a65c0c7bccf2830
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-01-17 17:02:32 -0500

    Create 0457. Classical Binary Search.java

---
+++ 0457. Classical Binary Search.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int findPosition(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
